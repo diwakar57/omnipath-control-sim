6a2c491f9fd19fe1c80d3b0d1e23470ab9eb9d1c
Diwakar Khadka <khadka.57@wright.edu>
2026-04-06 15:43:23 -0400

    Add Path class for path generation and error calculation

---
+++ utils/path_generator.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+class Path:
+    def __init__(self, start_point, end_point):
+        self.start = np.array(start_point)
+        self.end = np.array(end_point)
+        
+    def get_closest_point(self, robot_pos):
+        # For a straight line along the X-axis (y=0)
+        # The goal is simply the projection of the robot onto the line
+        return np.array([robot_pos[0], self.start[1]])
+
+    def get_cte(self, robot_pos):
+        # Cross-Track Error: Vertical distance from the line y = start_y
+        return robot_pos[1] - self.start[1]
